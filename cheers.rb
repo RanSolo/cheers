dc42c9b7a7dcc6d31fec04e5130dc6fa9bff0292
Randy Russell <ransolo@me.com>
2014-04-16 13:59:38 -0500

    got my cheers.rb from home

--- cheers.rb
+++ cheers.rb
@@ -1,12 +1,18 @@
-# Build on the results of our in-class exploration to output, for example:
-# Give me an... A
-# Give me a... B
-# Give me a... B
-# Give me a... Y
-# ABBY’s just GRAND!
-# When given the input of “Abby”. Note: the “a” vs. “an”
+# From IRb
+#my_array = ["foo", "bar", "grille"]
 
-name = gets.chomp
-name.each_char do |char|
-  puts "Give me a.. #{char.upcase}"
+#my_array.each do |item|
+#  puts item.capitalize
+#end
+
+puts "What's your name?"
+name = gets.chomp.downcase
+
+name.each_char do |c|
+  if c["a"] || c["e"] || c["i"] || c["o"] || c["r"]
+    puts "Give me an... " + c.upcase
+  else
+    puts "Give me a... " + c.upcase
+  end
 end
+puts name.upcase  + "'s just GRAND!"
